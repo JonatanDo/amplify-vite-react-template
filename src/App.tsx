35450e4d6705b075d2018124330573653d5b8c21
JONATAN DOMENECH <46609077+JONATANDOMENECH@users.noreply.github.com>
2024-07-10 13:20:14 +0200

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,10 @@ function App() {
       next: (data) => setTodos([...data.items]),
     });
   }, []);
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
@@ -22,9 +26,11 @@ function App() {
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
+        {todos.map((todo) => <li 
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+          {todo.content}
+        </li>)}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
